648a15ef58e817c9e2c066b412f61f133e88b013
saadidhia <saadidhia3@gmail.com>
2021-10-21 04:58:58 -0700

    First Commit after Creating Spring Initializer Project

---
+++ src/main/java/com/kafka/KafkaSampleMicroservice/KafkaSampleMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.KafkaSampleMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSampleMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSampleMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafka/KafkaSampleMicroservice/KafkaSampleMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.KafkaSampleMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSampleMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
