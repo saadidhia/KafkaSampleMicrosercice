ff67c851107a027b91fde71fab661008842f9984
saadidhia <saadidhia3@gmail.com>
2021-10-25 07:34:57 -0700

    Instance of Consumer

--- src/main/java/com/kafka/KafkaSampleMicroservice/KafkaController.java
+++ src/main/java/com/kafka/KafkaSampleMicroservice/KafkaController.java
@@ -1,24 +1,36 @@
 package com.kafka.KafkaSampleMicroservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
+
 @RestController
 @RequestMapping("/api/kafka")
 public class KafkaController {
 	
 	@Autowired
-	private KafkaTemplate<String,MyModel> kafkaTemplate;
+	private Gson gsonConverter;
+	
+	@Autowired
+	private KafkaTemplate<String,String> kafkaTemplate;
 	
 	
 	@PostMapping("post")
 	public void PostMessage(@RequestBody MyModel myModel) {
 		System.out.println("invoked");
-	 kafkaTemplate.send("myTopic",myModel);
+	 kafkaTemplate.send("myTopic",gsonConverter.toJson(myModel));
+	}
+	
+	
+	@KafkaListener(topics="myTopic", groupId = "test-consumer-group")
+	public void kafkaGet(String myModel) {
+		System.out.println(myModel.toString());
 	}
 	
 	
--- src/main/java/com/kafka/KafkaSampleMicroservice/MyModel.java
+++ src/main/java/com/kafka/KafkaSampleMicroservice/MyModel.java
@@ -3,15 +3,15 @@ package com.kafka.KafkaSampleMicroservice;
 public class MyModel {
 	
 	private String field1;
-	private String filed2;
+	private String field2;
 	
 	
 	public MyModel() {
 	}
-	public MyModel(String field1, String filed2) {
+	public MyModel(String field1, String field2) {
 		super();
 		this.field1 = field1;
-		this.filed2 = filed2;
+		this.field2 = field2;
 	}
 	public String getField1() {
 		return field1;
@@ -19,11 +19,11 @@ public class MyModel {
 	public void setField1(String field1) {
 		this.field1 = field1;
 	}
-	public String getFiled2() {
-		return filed2;
+	public String getField2() {
+		return field2;
 	}
-	public void setFiled2(String filed2) {
-		this.filed2 = filed2;
+	public void setField2(String field2) {
+		this.field2 = field2;
 	}
 	
 	
